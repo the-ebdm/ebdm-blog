a2f3bd745509bda5bf0f8832dee0e790c85c4472
Eric Muir <eric.muir@xdesign.com>
2022-05-22 20:08:51 +0100

    Move tracking script to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script 
+      async
+      defer
+      data-website-id="a6cc6ca8-3376-4035-8f2a-fbed75fdf1e8"
+      src="https://umami-production-7b7f.up.railway.app/umami.js" />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
